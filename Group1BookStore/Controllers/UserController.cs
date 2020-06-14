778cf64da327949587a71cca83b2f923444f1a6c
Jake <jakeheld123@gmail.com>
2020-06-13 22:59:26 -0500

    added user controller removed unused dtos

--- Group1BookStore/Controllers/UserController.cs
+++ Group1BookStore/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Group1BookStore.Data.Entities;
 using Group1BookStore.Features.Users.Dtos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,7 @@ namespace Group1BookStore.Controllers
             this.dataContext = dataContext;
         }
 
-        /*[HttpPost]
+        [HttpPost]
         [Route("create")]
         public async Task<ActionResult<UserDto>> CreateUser(UserCreateDto dto)
         {
@@ -35,8 +36,35 @@ namespace Group1BookStore.Controllers
             // wrapping in a transaction means that if part of the transaction fails, then everything saved is undone
             using(var transaction = await dataContext.Database.BeginTransactionAsync())
             {
-                if(string.Equals(dto.Role, Roles.Customer))
+                if(string.Equals(dto.Role, Roles.Customer, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return BadRequest(new { message = "Employees should not be able to create Customers." });
+                }
+
+                if(!await dataContext.Roles.AnyAsync(x => x.Name == dto.Role))
+                {
+                    return BadRequest(new { message = "The role you are trying to assing does not exist." });
+                }
+
+                var identityResult = await userManager.AddToRoleAsync(newUser, dto.Password);
+                if (!identityResult.Succeeded)
+                {
+                    return BadRequest(new { message = "Unable to create user with given Username and Password. Verify Password meets specifications." });
+                }
+
+                var roleResult = await userManager.AddToRoleAsync(newUser, dto.Role);
+                if (!roleResult.Succeeded)
+                {
+                    return BadRequest(new { message = "Unable to add role to desired user. Verify that Role exists." });
+                }
+
+                transaction.Commit(); // This marks our work is done, and sends information to the database.
+
+                return Created(string.Empty, new UserDto
+                {
+                    Username = newUser.UserName
+                });
             }
-        }*/
+        }
     }
 }
